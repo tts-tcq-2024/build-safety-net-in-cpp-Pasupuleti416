86c513522e313eff247b4e688f3cc69acc0b3b26
Pasupuleti416 <pasupuletiharitha68@gmail.com>
2024-09-11 23:31:55 +0530

    Update Soundex.Tests.cpp

--- Soundex.Tests.cpp
+++ Soundex.Tests.cpp
@@ -10,3 +10,15 @@ TEST(SoundexTest, HandlesSingleCharacter) {
     EXPECT_EQ(generateSoundex("A"), "A000");
 }
 
+TEST(SoundexTest, HandlesSingleCharacter) {
+    EXPECT_EQ(generateSoundex("Robert"), "R163");
+}
+
+TEST(SoundexTest, HandlesSingleCharacter) {
+    EXPECT_EQ(generateSoundex("Ashcroft"), "A261");
+}
+
+TEST(SoundexTest, HandlesSingleCharacter) {
+    EXPECT_EQ(generateSoundex("Honeyman"), "H555");
+}
+
